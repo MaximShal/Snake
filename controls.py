c4cf9f3cf495687e85de0af7dcae7d385a5001e8
MaximSh <baksbanni31@gmail.com>
2023-01-27 18:06:02 +0200

    first commit

--- controls.py
+++ controls.py
@@ -2,10 +2,11 @@ import pygame
 import sys
 
 
-def events(snake):
+def events(snake, bot):
     # Івенти натискання кнопок, для тесту(можна погратись)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            bot.end(False)
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
@@ -27,13 +28,6 @@ def update(bg_color, screen, snake, food):
 
 
 def check_collision(snake, food):
-    if (snake.x1 in list(range(food.food_x, food.food_x + 10)) and snake.y1 in list(
-            range(food.food_y, food.food_y + 10)) or
-            snake.x1 + 10 in list(range(food.food_x, food.food_x + 10)) and snake.y1 in list(
-                range(food.food_y, food.food_y + 10)) or
-            snake.x1 in list(range(food.food_x, food.food_x + 10)) and snake.y1 + 10 in list(
-                range(food.food_y, food.food_y + 10)) or
-            snake.x1 + 10 in list(range(food.food_x, food.food_x + 10)) and snake.y1 + 10 in list(
-                range(food.food_y, food.food_y + 10))):
+    if len(snake.snake_head_dots.intersection(food.block)) != 0:
         food.create_food()
         snake.increase()
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 import controls
-from snake import Snake, Food
+from snake import Snake
+from food import Food
+from bot import Bot
 import time
+from threading import Thread
 
 
 def run():
@@ -12,14 +15,17 @@ def run():
     pygame.display.set_caption('Snake')
     bg_color = (0, 0, 0)
     font_style = pygame.font.SysFont('Arial', 50)
+
     snake = Snake(screen, screen_width, screen_height)
     food = Food(snake, screen)
     snake.update_snake()
     food.create_food()
-
+    bot = Bot()
+    thread = Thread(name='bot', target=bot.play)
+    thread.start()
 
     while True:
-        controls.events(snake)
+        controls.events(snake, bot)
         snake.update_snake()
         controls.check_collision(snake, food)
         controls.update(bg_color, screen, snake, food)
@@ -28,6 +34,7 @@ def run():
             msg = font_style.render('DEAD', True, (255, 255, 255))
             screen.blit(msg, [screen_width/2-50, screen_height/2-30])
             pygame.display.update()
+            bot.end(False)
             time.sleep(2)
             break
 
